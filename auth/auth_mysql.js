d6badfea653eab02057c4b8fea795211c1bc2610
Jeremy Cowgar <jeremy@cowgar.com>
2014-04-21 11:18:07 -0400

    Working authentication off MySQL server

---
+++ auth/auth_mysql.js
@@ -0,0 +1,70 @@
+// Authentication against a MySQL server
+var net_utils = require('./net_utils');
+var mysql = require('mysql');
+var crypto = require('crypto');
+
+exports.register = function() {
+	this.inherits('auth/auth_base');
+}
+
+exports.hook_capabilities = function(next, connection) {
+	// Do not allow AUTH unless private IP or encrypted
+	if (!net_utils.is_rfc1918(connection.remote_ip) && !connection.using_tls) {
+		return next();
+	}
+
+	var methods = [ 'PLAIN' ];
+	connection.capabilities.push('AUTH ' + methods.join(' '));
+	connection.notes.allowed_auth_methods = methods;
+
+	return next();
+}
+
+exports.get_plain_passwd = function(user, cb) {
+	if (!server.notes.auth_mysql || !server.notes.auth_mysql.conn) {
+		var config = this.config.get('auth_mysql.ini', {
+			host: 'localhost',
+			port: 3306,
+			char_set: 'UTF8_GENERAL_CI',
+			ssl: false,
+			password_query: 'SELECT password FROM users WHERE user=?'
+		});
+
+		server.notes.auth_mysql = {};
+		server.notes.auth_mysql.config = config;
+		server.notes.auth_mysql.conn = mysql.createConnection({
+			host : config.main.host,
+			port : config.main.port,
+			charset: config.main.charset,
+			user : config.main.user,
+			password: config.main.password,
+			database: config.main.database,
+		});
+		server.notes.auth_mysql.conn.connect();
+	}
+
+	var myNotes = server.notes.auth_mysql;
+	myNotes.conn.query(myNotes.config.main.password_query, [user], function(err, results) {
+		if (results && results.length > 0) {
+			cb(results[0].password);
+		} else {
+			cb();
+		}
+	});
+}
+
+exports.check_plain_passwd = function (connection, user, passwd, cb) {
+	var plugin = this;   
+	this.get_plain_passwd(user, function (plain_pw) {
+		if (plain_pw === null) {
+			return cb(false);
+		}
+
+		var passwd_md5_enc = crypto.createHash('md5').update(passwd).digest('hex');
+		if (plain_pw === passwd_md5_enc) {
+			return cb(true);
+		}
+
+		return cb(false);
+	});
+}
